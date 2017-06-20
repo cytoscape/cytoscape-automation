610ece067b6cbdb571175a9efca5d36709fa2285
David Otasek <dotasek.dev@gmail.com>
2017-06-20 15:05:56 -0400

    Updated links, and updated best practices python test

--- for-app-developers/cy-automation-cy-rest-best-practices-sample/python_tests/tests.py
+++ for-app-developers/cy-automation-cy-rest-best-practices-sample/python_tests/tests.py
@@ -16,13 +16,13 @@ Execution requirements:
 
 You should get console output that says looks something like the folllowing:
   
-  Ran 1 test in 0.042s
+  Ran 2 tests in 0.042s
   
   OK
   
   Process finished with exit code 0
 
-This indicates that your app has passed the two tests we have written below.
+This indicates that your app has passed the 2 tests we have written below.
 
 """
 
@@ -40,7 +40,8 @@ class SampleTestCase(unittest.TestCase):
         pass
 
     # Any code that needs to be run after each test should go here.
-    # In our case, we remove the only student we add in these tests so subsequent calls have the same initial state.
+    # In our case, the test_students() method adds a specific student, Jeff Winger, to the student list, and this should
+    # be removed to return the resource to its original state.
     def tearDown(self):
         result = requests.request("GET",
                                   "http://" + SampleTestCase._HOST + ":" + SampleTestCase._PORT + "/cyrestbestpractices/v1/classroom/students",
@@ -67,9 +68,9 @@ class SampleTestCase(unittest.TestCase):
                                 headers=SampleTestCase._HEADERS)
 
     """
-    This is a test of basic functionality. 
-    
     All tests in our test suite are named starting with the text 'test'. This is set up in the suite() definition.
+    
+    This is a test of the GET /cyrestbestpractices/v1/classroom/teacher operation.
     """
     @staticmethod
     def test_get_teacher():
@@ -99,11 +100,17 @@ class SampleTestCase(unittest.TestCase):
         assert lastName == "Chang", "Expected lastName to be 'Chang' but was '" + lastName + "'"
         assert age == 32, "Expected age to be 32, but was {}".format(age)
 
+    """
+    This is a test of the POST /cyrestbestpractices/v1/classroom/students operation.
+    
+    We get a list of all pre-existing students that we can make a simple size comparison against, add a new student, and
+    then check that the size of the student list has incremented by one.
+    """
     @staticmethod
     def test_students():
         # Set up HTTP Request headers for our request.
 
-        # Perform the request
+        # Make a get request to get a list of current students.
         result = requests.request("GET",
                                   "http://" + SampleTestCase._HOST + ":" + SampleTestCase._PORT + "/cyrestbestpractices/v1/classroom/students",
                                   data=None,
@@ -117,7 +124,10 @@ class SampleTestCase(unittest.TestCase):
         #Noting the original number of students
         originalLength = len(students);
 
-        jsonMessageBody = json.dumps({"firstName": "Jeff","lastName":"Winger", "age": 43})
+        #Creating a JSON object for the new student.
+        jsonMessageBody = json.dumps({"firstName": "Jeff","lastName":"Winger", "age": 42})
+
+        #Making a post request to add the student.
         result = requests.request("POST",
                                   "http://" + SampleTestCase._HOST + ":" + SampleTestCase._PORT + "/cyrestbestpractices/v1/classroom/students",
                                   data=jsonMessageBody,
@@ -126,6 +136,7 @@ class SampleTestCase(unittest.TestCase):
         assert result.status_code == codes.OK, "Status code was expected to be 200, but was {}".format(
             result.status_code)
 
+        # Make a get request to get the updated list of students.
         result = requests.request("GET",
                                   "http://" + SampleTestCase._HOST + ":" + SampleTestCase._PORT + "/cyrestbestpractices/v1/classroom/students",
                                   data=None,
@@ -139,7 +150,11 @@ class SampleTestCase(unittest.TestCase):
         # Assert that adding a student increased the number of students by one.
         assert originalLength+1 == len(newStudents), "Expected student size to be {} but was {}".format((originalLength+1), len(students))
 
-        # The following checks to make sure that we now have a the exact student we added in the classroom.
+        """
+        The following lines of code check to make sure that we now have a the exact student we added in the classroom by
+        iterating over the student list, using a GET to retrieve each individual student, and setting hasJeffWinger to 
+        true if a student with the same fields as the one we added are found. 
+        """
         hasJeffWinger = False
 
         for student in newStudents:
@@ -149,9 +164,10 @@ class SampleTestCase(unittest.TestCase):
                                       params=None,
                                       headers=SampleTestCase._HEADERS)
             studentJson = studentResult.json()
-            if studentJson["firstName"] == "Jeff" and studentJson["lastName"] == "Winger":
+            if studentJson["firstName"] == "Jeff" and studentJson["lastName"] == "Winger" and studentJson["age"] == 42:
                 hasJeffWinger = True
 
+        #This should fail if we didn't find a student named Jeff Winger.
         assert hasJeffWinger, "POST student operation did not add Jeff Winger."
 
 
