49bee5878f159b8f7984bcf79ed8470b94cef391
Alexander Pico <apico@gladstone.ucsf.edu>
2017-06-22 19:26:29 -0700

    new function for r2cytoscape

---
+++ for-scripters/R/r2cytoscape/R/getNetworkSuid.R
@@ -0,0 +1,15 @@
+#' Get the network suid
+#'
+#' @param network.name name of the network that you want the suid; default is "current" network
+#' @param base.url cyrest base url for communicating with cytoscape
+#' @return network viewid
+#' @export
+#' @import stringr
+
+getNetworkSuid <- function(network.name='current', base.url='http://localhost:1234/v1'){
+    cmd<-paste0('network get attribute network="',network.name,'" namespace="default" columnList="SUID"')
+    res <- commandRun(cmd)
+    network.suid <- str_extract(res,"[0-9]+")
+    return(network.suid)
+}
+
