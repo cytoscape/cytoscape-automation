f8cbc9cb33ed26be307109d9d37038bb5d2e86a9
Alexander Pico <apico@gladstone.ucsf.edu>
2018-12-13 18:25:29 -0800

    removing outdated content

--- for-scripters/R/r2cytoscape/R/listNetworks.R
+++
@@ -1,9 +0,0 @@
-#' List the names of all networks in current session
-#'
-#' @param base.url cyrest base url for communicating with cytoscape
-#' @return list of network names
-#' @export
-
-listNetworks<-function(base.url='http://localhost:1234/v1'){
-    commandRun('network list')
-}
--- for-scripters/R/r2cytoscape/README.md
+++
@@ -1,3 +0,0 @@
-### Old location for r2cytoscape
-
-Please see https://github.com/cytoscape/r2cytoscape for latest version and development
